074ae9234ee48577c7c131ed5b4694628d0cdefa
rofifatuzulfa <rofifatuz.rofzul@gmail.com>
2022-08-31 19:43:43 +0700

    release 2

--- src/components/cards.js
+++ src/components/cards.js
@@ -6,8 +6,8 @@ export default class Cards extends Component {
     return (
       <div className='container'>
         <div className='container-card'>
-            <img className='image' src={this.props.image} />
-            <div>
+            <img className='image' src={this.props.image} alt=''/>
+            <div className='title'>
                 <h4>{this.props.title}</h4>
             </div>
         </div>
